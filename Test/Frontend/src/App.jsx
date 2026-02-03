90e6f183ad024f0d3ee8b92da8b371d97228ded2
Anurag singh <anurag2307singh@gmail.com>
2026-02-03 15:48:44 +0530

    added otp

--- Test/Frontend/src/App.jsx
+++ Test/Frontend/src/App.jsx
@@ -1,26 +1,37 @@
-import React, { useState } from "react";
+import React, { useRef, useState } from 'react'
 
-const App = ({ length = 5 }) => {
-  const [arr, setArr] = useState(() => new Array(length).fill(""));
-
-  function inputHandler(e, index) {
-    const value = e.target.value;
-    setArr(prev => {
-      const copy = [...prev];
-      copy[index] = value;
-      return copy;
-    });
-  }
+const App = ({length=5}) => {
+    const [input,setInput]=useState(()=>(new Array(length).fill(" ")))
+    const inputRef= useRef([]);
+    function handleChange(e,index){
+      const {value}=e.target;
+      const res=value.slice(value.length-1);
+      const check=/^[0-9]$/.test(res);
+      if(check){
+      setInput((prev)=>{
+        prev[index]=res;
+        return [...prev];
+      })
+      if(index <input.length-1)
+    {
+      inputRef.current[index+1].focus()
+    }
+      
+    }
+    
+    return;
+    }
 
   return (
     <div className="container">
-      {arr.map((item, index) => (
+      {input.map((item, index) => (
         <input
-          key={index}
-          value={item}
-          onChange={(e) => inputHandler(e, index)}
           type="text"
-          className="box"
+          key={index}
+          ref={(e)=>inputRef.current[index]=e}
+          className="input"
+          value={item}
+          onChange={(e) => handleChange(e, index)}
         />
       ))}
     </div>
