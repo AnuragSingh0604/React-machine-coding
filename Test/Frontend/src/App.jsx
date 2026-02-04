f61e4e6fc1bee2baf30e33e3377a33713212a8d6
Anurag singh <anurag2307singh@gmail.com>
2026-02-04 13:08:24 +0530

    added otp

--- Test/Frontend/src/App.jsx
+++ Test/Frontend/src/App.jsx
@@ -1,7 +1,7 @@
-import React, { useRef, useState } from 'react'
+import React, { useRef, useState ,useEffect} from 'react'
 
 const App = ({length=5}) => {
-    const [input,setInput]=useState(()=>(new Array(length).fill(" ")))
+    const [input,setInput]=useState(()=>(new Array(length).fill("")))
     const inputRef= useRef([]);
     function handleChange(e,index){
       const {value}=e.target;
@@ -21,6 +21,44 @@ const App = ({length=5}) => {
     
     return;
     }
+    function handleKeydown(e,index){
+      e.preventDefault;
+
+     
+      const {key}=e;
+    if(key!=="Backspace" && key!=="ArrowLeft" && key!=="ArrowRight")
+    {
+      return;
+    }
+    if(index >0 && key==="Backspace"){
+      if(input[index]==="")
+      {
+         inputRef.current[index-1].focus();
+      }
+     setInput((prev)=>{
+      prev[index]="";
+      return [...prev];
+
+     }
+      )
+      return;
+     
+    }
+    if( index >0 && key==="ArrowLeft"){
+          inputRef.current[index-1].focus();
+          return;
+    }
+    if( index<input.length-1 && key==="ArrowRight"){
+          inputRef.current[index+1].focus();
+          return;
+    }
+    return;
+    
+    }
+    useEffect(() => {
+  inputRef.current[0]?.focus();
+}, []);
+
 
   return (
     <div className="container">
@@ -31,6 +69,10 @@ const App = ({length=5}) => {
           ref={(e)=>inputRef.current[index]=e}
           className="input"
           value={item}
+          inputMode="numeric"
+      pattern="[0-9]*"
+      autoComplete="one-time-code"
+          onKeyDown={(e)=>handleKeydown(e,index)}
           onChange={(e) => handleChange(e, index)}
         />
       ))}
