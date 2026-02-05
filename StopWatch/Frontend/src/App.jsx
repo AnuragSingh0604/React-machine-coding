dbe8671f2a3bfbdb22ed5d3d859543c4dfc5d2a0
Anurag singh <anurag2307singh@gmail.com>
2026-02-05 18:28:27 +0530

    added stopwatch

---
+++ StopWatch/Frontend/src/App.jsx
@@ -0,0 +1,60 @@
+import React, { useState ,useRef} from 'react'
+
+const App = () => {
+  const [box,setBox]=useState(()=>new Array(3).fill(0));
+  const intervalId=useRef(null);
+  const boxRef=useRef([]);
+  function starthandler(){
+      if(intervalId.current){
+        return;
+      }
+       intervalId.current=setInterval(()=>{
+        setBox((prev)=>{
+          const copy=[...prev];
+        
+          copy[2]=copy[2]+1;
+            if(copy[2] >= 60){
+              copy[2]=0;
+              copy[1]=copy[1]+1;
+
+            }
+            if(copy[1]>=60){
+              copy[1]=0;
+              copy[0]=copy[0]+1;
+
+            }
+          return copy;
+        })
+
+    },1000);
+       
+  }
+  function stophandler(){
+   clearInterval(intervalId.current);
+   intervalId.current=null;
+  }
+  return (
+    <>
+   <div className='container'>
+    {
+      box.map((item,index)=>{
+       
+       
+         return  <><div  ref={(e)=>boxRef.current[index]=e} key ={index} className='box'>{item}</div>
+             {index < box.length-1 && <p>:</p>}
+              
+            </>
+           
+      })
+    }
+
+   </div>
+   <div className='buttonContainer'>
+    <button onClick={()=>starthandler()}>start</button>
+     <button onClick={stophandler}>stop</button>
+     </div>
+     </>
+  )
+}
+
+export default App
