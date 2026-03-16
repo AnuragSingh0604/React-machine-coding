774d83fad32e3cff0c6e2a9020323edf86166dc5
Anurag singh <anurag2307singh@gmail.com>
2026-03-16 14:06:04 +0530

    added seo tag

--- Test/Frontend/src/Pagination.jsx
+++ Test/Frontend/src/Pagination.jsx
@@ -1,7 +1,11 @@
 import React from 'react'
 
 const Pagination = ({page,setPage}) => {
-    let totalPage=15;
+   let totalPage=15;
+  const prevThreepage=Array.from({length:3},(item,index)=>page-1-index).reverse().filter((item,index)=>item>0);
+   const nextFourPage=Array.from({length:4},(item,index)=>page+index).filter((item,index)=>item<=totalPage);
+  const allPage=[...prevThreepage,...nextFourPage];
+   
     function prevHandler(){
         if(page<=1){
             return;
@@ -19,6 +23,9 @@ const Pagination = ({page,setPage}) => {
 
     <div className='paginationContainer'>
       <button onClick={prevHandler}>&lt;</button>
+      {
+        allPage.map((value,index)=><button className={page===value?"active":""} onClick={()=>setPage(value)} key={index}>{value}</button>)
+      }
       
       <button onClick={nextHandler}>&gt;</button>
 
