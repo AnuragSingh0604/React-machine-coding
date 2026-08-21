05c8710c21a47cf058165c73fd8eabcc4efc5e2f
AnuragSingh0604 <anurag2307singh@gmail.com>
2026-08-21 07:53:03 +0530

    vir list

--- Debounce/Frontend/src/Hooks/useDebounce.jsx
+++ Debounce/Frontend/src/Hooks/useDebounce.jsx
@@ -1,10 +1,18 @@
-import React from 'react'
+import React ,{useEffect, useState}from 'react'
 
 const useDebounce = (value,delay) => {
-    
-  return (
-    <div>useDebounce</div>
-  )
+    const [newValue,setNew] = useState(null);
+    useEffect(()=>{
+        const timeoutId=setTimeout(()=>{
+           
+            setNew(value);
+        },delay);
+    return ()=>clearTimeout(timeoutId);
+
+    },[value,delay]);
+
+
+  return newValue;
 }
 
-export default useDebounce
+export default useDebounce;
